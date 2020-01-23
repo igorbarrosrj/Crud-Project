6d8fcf12178d5ba3b5ab32be8e0c105b869ea302
Igor Barros <igorbarrosrj@gmail.com>
2020-01-23 14:46:15 -0300

    Edição do Crud completo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 const mysql = require ('mysql')
+const bodyParser = require ('body-parser')
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
@@ -16,6 +17,8 @@ const dependencies = {
 }
 
 const pessoas = require('./routes/pessoas')
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static('public'))
 app.get('/', (req, res)=> res.render('home'))
--- routes/pessoas.js
+++ routes/pessoas.js
@@ -7,7 +7,12 @@ const pessoasRouter = ({connection}) => {
     const router = express.Router()
 
     router.get('/', pessoasController.index.bind(null, connection))
-    
+    router.get('/delete/:id', pessoasController.deleteOne.bind(null, connection))
+    router.get('/create', pessoasController.createForm)
+    router.post('/create', pessoasController.createProcess.bind(null, connection))
+    router.get('/update/:id', pessoasController.updateForm.bind(null,connection))
+    router.post('/update/:id', pessoasController.updateProcess.bind(null,connection))
+
     return router
 }
 
